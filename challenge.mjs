1d270910ec7d6d5bf2401d58a10105473b2a166e
Omran Jamal <o.jamal97@gmail.com>
2023-08-02 20:35:07 +0600

    challenge.init()

---
+++ challenge.mjs
@@ -0,0 +1,12 @@
+const weirdExpressions = [
+  "a othoba b",
+  "b ebong c othoba d",
+  "ebong ebong othoba othoba ebong",
+  "((ebong) othoba ebong) ebong othoba",
+  "(ebong othoba (ebong ebong ((othoba)othoba(ebong))))",
+  "ebong",
+];
+
+for (const expression of weirdExpressions) {
+  // fill this in
+}
